92257e8163b4bbe7fb76ea760873bbcf121ee9ca
sky14786 <39879299+sky14786@users.noreply.github.com>
2019-04-25 09:27:12 +0900

    backup

---
+++ 04-25/src/com/kh/day3/Run.java
@@ -0,0 +1,9 @@
+package com.kh.day3;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ 04-25/src/com/kh/day3/fucntion/CastingTest.java
@@ -0,0 +1,11 @@
+package com.kh.day3.fucntion;
+
+public class CastingTest {
+	public void castingTest() {
+		//자동형변환
+		int account=1000;
+		long accountVip;
+		
+		accountVip = account;
+	}
+}
