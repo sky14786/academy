b8d3a52c9b57ca2d17bb84422b0b280d3ede53eb
sky14786 <39879299+sky14786@users.noreply.github.com>
2019-04-25 14:48:49 +0900

    backup

--- 04-25/src/com/kh/day3/Run.java
+++ 04-25/src/com/kh/day3/Run.java
@@ -2,12 +2,14 @@ package com.kh.day3;
 
 //import com.kh.day3.fucntion.CastingTest;
 import com.kh.day3.fucntion.ScannerTeset;
+import com.kh.day3.fucntion.OperatorTest;
 
 public class Run {
 
 	public static void main(String[] args) {
 		// new CastingTest().castingTest();
-		new ScannerTeset().inputTest();
+//		new ScannerTeset().inputTest();
+		new OperatorTest().selfAdd();
 	}
 
 }
---
+++ 04-25/src/com/kh/day3/fucntion/OperatorTest.java
@@ -0,0 +1,9 @@
+package com.kh.day3.fucntion;
+
+public class OperatorTest {
+	public void selfAdd() {
+		int num = 10;
+		num++;
+		System.out.println(num);
+	}
+}
