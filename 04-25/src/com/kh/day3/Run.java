2246aa6ebbbe21c3feadd69f58fb7aeda35ad5d9
sky14786 <39879299+sky14786@users.noreply.github.com>
2019-04-25 09:36:21 +0900

    backup

--- 04-25/src/com/kh/day3/Run.java
+++ 04-25/src/com/kh/day3/Run.java
@@ -1,9 +1,11 @@
 package com.kh.day3;
 
+import com.kh.day3.fucntion.CastingTest;
+
 public class Run {
 
 	public static void main(String[] args) {
-
+		new CastingTest().castingTest();
 	}
 
 }
--- 04-25/src/com/kh/day3/fucntion/CastingTest.java
+++ 04-25/src/com/kh/day3/fucntion/CastingTest.java
@@ -2,10 +2,22 @@ package com.kh.day3.fucntion;
 
 public class CastingTest {
 	public void castingTest() {
-		//자동형변환
-		int account=1000;
+		// 자동형변환
+		int account = 1000;
 		long accountVip;
-		
+
 		accountVip = account;
+		// account 는 int형이고 accountVip는 long형으로 자료형이 다르나
+		// accountVip에 대입되어지는 account는 accountVip보다 크기가 작은 자료형이기 때문에
+		// 컴파일러가 자동으로 형변환을 해주기 때문에 대입이 가능하게 된다.
+		// 계산시에도 자동형변환이 된다.
+		int su = 10;
+		double height = 180.5;
+		double result = su + height;
+		
+		char ch='A';
+		int character =ch;
+		System.out.println(character);
+
 	}
 }
