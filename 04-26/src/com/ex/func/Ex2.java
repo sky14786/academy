27fbee6871094a4d64a2bc88079de581699cc036
sky14786 <39879299+sky14786@users.noreply.github.com>
2019-04-26 14:30:27 +0900

    backup

---
+++ 04-26/src/com/ex/func/Ex2.java
@@ -0,0 +1,23 @@
+package com.ex.func;
+
+import java.util.Scanner;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		thirdOper();
+	}
+
+	public static void thirdOper() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("첫번째 수 입력 : ");
+		int first = sc.nextInt();
+		System.out.print("두번째 수 입력 : ");
+		int second = sc.nextInt();
+
+		String msg = first > second ? "첫번째 수가 커" : "두번째 수가 커";
+
+		System.out.println(msg);
+
+	}
+}
