e0cca415a1bc3a0150ea3c577c4f5ad290d5ca96
sky14786 <39879299+sky14786@users.noreply.github.com>
2019-04-26 09:30:20 +0900

    backup

---
+++ 04-26/src/com/kh/day4/func/OperatorTest.java
@@ -0,0 +1,14 @@
+package com.kh.day4.func;
+
+public class OperatorTest {
+	public void cal() {
+		// 기본적인 사칙연산하기
+		int num = 10;
+		int num2 = 20;
+		int num3 = 30;
+
+		System.out.println(num + num2);
+		System.out.println(num2 + num3);
+		System.out.println(num + num2 + num3);
+	}
+}
