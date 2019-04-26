1258e0adb6f6bc80a6b99a8586dad1ee6de2cfb1
sky14786 <39879299+sky14786@users.noreply.github.com>
2019-04-26 09:24:23 +0900

    04-26 backup

---
+++ 04-26/src/com/kh/day4/run/Main.java
@@ -0,0 +1,12 @@
+package com.kh.day4.run;
+
+import com.kh.day4.func.OperatorTest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		OperatorTest operatorTest = new OperatorTest();
+		operatorTest.cal();
+	}
+
+}
