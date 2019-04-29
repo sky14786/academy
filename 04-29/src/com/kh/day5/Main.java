85467bffa6b5515bd336a94c106b055f5a2efffe
sky14786 <39879299+sky14786@users.noreply.github.com>
2019-04-29 10:20:57 +0900

    back up

---
+++ 04-29/src/com/kh/day5/Main.java
@@ -0,0 +1,11 @@
+package com.kh.day5;
+
+import com.kh.day5.function.ConditionTest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new ConditionTest().condition();
+	}
+
+}
---
+++ 04-29/src/com/kh/day5/function/ConditionTest.java
@@ -0,0 +1,10 @@
+package com.kh.day5.function;
+
+public class ConditionTest {
+	public void condition() {
+		// 조건문 if를 사용해보자!
+		if (19 > 0) {
+			System.out.println("출력");
+		}
+	}
+}
