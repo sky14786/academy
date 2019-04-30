123b70ca2eab2837c7e27ebbc48f852e0e65b454
sky14786 <39879299+sky14786@users.noreply.github.com>
2019-04-30 09:10:27 +0900

    04-30 backup

--- test/src/test.java
+++ test/src/test.java
@@ -1,9 +1,5 @@
-public class test{
-    public static void main(String[] args){
-        System.out.println("|\\_/|");
-        System.out.println("|q p|   /}");
-        System.out.println("( 0 )\"\"\"\\");
-        System.out.println("|\"^\"`    |");
-        System.out.println("||_/=\\\\__|");
-    }
+public class test {
+	public static void main(String[] args) {
+
+	}
 }
