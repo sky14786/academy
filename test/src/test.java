5496fd5f215ce4ab7f052625539c20c4296c8992
sky14786 <39879299+sky14786@users.noreply.github.com>
2019-05-01 00:20:58 +0900

    backup

--- test/src/test.java
+++ test/src/test.java
@@ -1,71 +1,64 @@
 import java.util.Scanner;
 
-public class Test(){
+public class test {
 	public static void main(String[] args) {
-		//두 수를 입력받아 두 수 사이의 합을 구하고, 출력하시오.
-		
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("수 1 입력 : ");
+		System.out.print("문자열을 입력하시오 : ");
+		String str = sc.nextLine();
+		System.out.print("정수를 입력하시오 : ");
 		int num = sc.nextInt();
-		
-		//첫번째수가 작음
-		
-		System.out.print("수 2 입력 : ");
-		int num2 = sc.nextInt();
-		
-		int sum = 0 ;
-			
-			
-		
-		for (int i = num  ; num <num2 ; num++  )
-		{
-			
-		sum += i ;
-			
-		
+
+		int temp = 0;
+		for (int i = 0; i < str.length(); i++) {
+			temp = str.charAt(i);
+			// 소문자 97 ~ 122 (a~z) 대문자 65 ~ 90 A~Z
+			if (str.charAt(i) != ' ') {
+				num %= 26;
+				if (temp >= 65 && temp <= 90) {
+					temp += num;
+					if (temp > 90) {
+						temp -= 26;
+					}
+				} else if (temp >= 97 && temp <= 122) {
+					temp += num;
+					if (temp > 122) {
+						temp -= 26;
+					}
+				}
+				System.out.print((char) temp);
+			} else {
+				System.out.print(str.charAt(i));
+			}
+
 		}
-		
-		System.out.println(sum);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-////		System.out.print("문자열 하나를 입력하시오 : ");
-////		String str = sc.nextLine();
-//		System.out.print("0보다 큰 정수 하나 입력 : ");
-//		int num = sc.nextInt();
-//
-////		String temp = "i am hungry";
-////		temp.charAt(0);
-//		// -> i
-//
-//		// 지정된 문자 aBz - > bCa 로 출력이 되야함.
-//		// 소문자는 소문자로 대문자는 대문자로 되야대여
-//		String str = "aBz";
-//
-////		int temp1 = word1+num;
-//
-//		for (int i = 0; i <= 2; i++) {
-//			char word1 = str.charAt(i);
-//			System.out.println((char) (i + 1));
-//		System.out.println(149 % 25);
-		
-//		System.out.print("문자열 입력");
-//		String a=sc.nextLine();
-//		System.out.print("정수 입력");
-//		int b=sc.nextInt();
-//		for(int i=0;i<=a.length();i++) {
-//			char num=a.charAt(i);
-//			char newNum=(char)(num+b);
-//			System.out.print(newNum+" ");
-//			continue;
-//	}
-//	}
-}
+	}
+	  public void prac01() {
+	      Scanner sc = new Scanner(System.in);
+	      System.out.println("문장을 입력하세요 : ");
+	      String str = sc.nextLine();
+	      System.out.println("얼마나 밀거? ");
+	      int plus = sc.nextInt();
+	      int nmg = plus%26;
+	      int result = 0;
+	      
+	      for(int i=0; i<str.length(); i++) {
+	         int strCh = str.charAt(i);
+	         if((char)strCh!=' ') {
+	            if((strCh>=65&&strCh<=90)||(strCh>=97&&strCh<=122)) {
+	               result = str.charAt(i)+nmg;
+	               if((result>=65&&result<=90)||(result>=97&&result<=122)){
+	                  System.out.print((char)(result));
+	               }else {
+	                  result=result-26;
+	                  System.out.println((char)(result));
+	               }
+	            }else {
+	               System.out.println("알파벳만 입력하세요.");
+	            }
+	         }else{
+	            System.out.print((char)(strCh));
+	         }
+	      }
+	   }
+
 }
