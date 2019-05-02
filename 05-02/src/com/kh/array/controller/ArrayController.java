5f8fa7b42d14389529558f31c0706e65e12aac26
sky14786 <39879299+sky14786@users.noreply.github.com>
2019-05-02 13:30:56 +0900

    backup

--- 05-02/src/com/kh/array/controller/ArrayController.java
+++ 05-02/src/com/kh/array/controller/ArrayController.java
@@ -2,6 +2,24 @@ package com.kh.array.controller;
 
 public class ArrayController {
 	public void arrayTest() {
+		// 배열선언!
+		// 자료형[] 변수명 ;
+		int[] intArr;
+		char[] charArr;
+
+		intArr = new int[5];
+		System.out.println(intArr[2]);
+		
+		System.out.println(intArr);
+
+	}
+
+	public void arrayTest2() {
+		char[] ch = { '가', '나', '하', '라', '마', '바' };
+
+		ch[2] = '다';
+		System.out.println(ch[2]);
+		System.out.println(ch);
 		
 	}
 }
--- 05-02/src/com/kh/array/main/Main.java
+++ 05-02/src/com/kh/array/main/Main.java
@@ -1,9 +1,11 @@
 package com.kh.array.main;
 
+import com.kh.array.controller.ArrayController;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		new ArrayController().arrayTest();
 	}
 
 }
