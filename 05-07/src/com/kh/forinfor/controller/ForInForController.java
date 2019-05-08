1e40a06953c8b78b2de76dadb936e58ce1ed1ebe
sky14786 <39879299+sky14786@users.noreply.github.com>
2019-05-08 09:43:54 +0900

    backup

--- 05-07/src/com/kh/forinfor/controller/ForInForController.java
+++ 05-07/src/com/kh/forinfor/controller/ForInForController.java
@@ -120,4 +120,5 @@ public class ForInForController {
 			System.out.println();
 		}
 	}
+
 }
--- test/src/testetest.java
+++ test/src/testetest.java
@@ -15,9 +15,9 @@ public class testetest {
 		sum = 0;
 
 		for (int i = a; i <= b; i++) {
-			for(int j=1;j<=9;j++) {
-				if((i*j)%2!=0) {
-					System.out.println(i+" * "+j+" = "+(i*j));
+			for (int j = 1; j <= 9; j++) {
+				if ((i * j) % 2 != 0) {
+					System.out.println(i + " * " + j + " = " + (i * j));
 				}
 			}
 		}
